36a641cd6aae1678d1c599aa96a8d5d8e8fd286d
Daniel Rudolph <daniel@bsolut.com>
2016-10-13 11:52:43 +0200

    silense db tool

--- start_test_db.sh
+++ start_test_db.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo "setting up tmpfs mysql server, creating test database"
-mysql_install_db --datadir /dev/shm/mysql
-/etc/init.d/mysql start
+mysql_install_db --datadir /dev/shm/mysql 1>/dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "db init failed"
+    cat /var/log/mysql/error.log
+fi
+/etc/init.d/mysql start 1>/dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "db start failed"
+    cat /var/log/mysql/error.log
+fi
 mysql -e "CREATE DATABASE test"
 #for some reason the config entry is ignored, workaround
 mysql -e 'set global sql_mode=NO_ENGINE_SUBSTITUTION'
